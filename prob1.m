2fc5393b190147c2e6ab784a9e526a218eb540e8
Kyle Prestado <57376389+kylerossece@users.noreply.github.com>
2019-12-07 18:15:58 +0800

    Add files via upload

---
+++ prob1.m
@@ -0,0 +1,14 @@
+function x = prob1()
+f=(0:99);
+for n=1:100
+ if f(n)<=9;
+     f(n)=(f(n)^2)-7;
+ elseif f(n)>=10;
+         f(n)=f(n-10);
+end
+end
+stem(f);
+xlabel('x-axis')
+ylabel('y-axis')
+title('Problem 1')
+end
